75c4cc9ed4be696284a64c31783eec7fd2966244
Steffen <steffen.kopecki@gmail.com>
2021-04-30 10:32:17 +0200

    Add note with octave as PitchedNote type

--- lib/music/note.test.ts
+++ lib/music/note.test.ts
@@ -1,41 +1,140 @@
-import { Note, renderNote, parseBeats, sumBeats } from "./note";
+import {
+  Note,
+  PitchedNote,
+  renderNote,
+  parseBeats,
+  sumBeats,
+  toFlat,
+  toSharp,
+} from "./note";
+
+test("converts sharp to flat notes", () => {
+  expect(toFlat(Note.Csharp)).toEqual(Note.Dflat);
+  expect(toFlat(Note.Dsharp)).toEqual(Note.Eflat);
+  expect(toFlat(Note.Fsharp)).toEqual(Note.Gflat);
+  expect(toFlat(Note.Gsharp)).toEqual(Note.Aflat);
+  expect(toFlat(Note.Asharp)).toEqual(Note.Bflat);
+
+  expect(toFlat(Note.C)).toEqual(Note.C);
+  expect(toFlat(Note.Aflat)).toEqual(Note.Aflat);
+});
+
+test("converts sharp to flat notes", () => {
+  expect(toSharp(Note.Dflat)).toEqual(Note.Csharp);
+  expect(toSharp(Note.Eflat)).toEqual(Note.Dsharp);
+  expect(toSharp(Note.Gflat)).toEqual(Note.Fsharp);
+  expect(toSharp(Note.Aflat)).toEqual(Note.Gsharp);
+  expect(toSharp(Note.Bflat)).toEqual(Note.Asharp);
+
+  expect(toSharp(Note.C)).toEqual(Note.C);
+  expect(toSharp(Note.Asharp)).toEqual(Note.Asharp);
+});
+
+test("converts sharp to flat notes", () => {
+  expect(toFlat(Note.Csharp)).toEqual(Note.Dflat);
+  expect(toFlat(Note.Gsharp)).toEqual(Note.Aflat);
+  expect(toFlat(Note.Asharp)).toEqual(Note.Bflat);
+  expect(toFlat(Note.C)).toEqual(Note.C);
+  expect(toFlat(Note.Aflat)).toEqual(Note.Aflat);
+});
 
 test("renders notes", () => {
-  expect(renderNote(Note.C)).toBe("C");
-  expect(renderNote(Note.Csharp)).toBe("C♯");
-  expect(renderNote(Note.A)).toBe("A");
-  expect(renderNote(Note.Aflat)).toBe("A♭");
-  expect(renderNote(Note.B)).toBe("B");
+  expect(renderNote(Note.C)).toEqual("C");
+  expect(renderNote(Note.Csharp)).toEqual("C♯");
+  expect(renderNote(Note.A)).toEqual("A");
+  expect(renderNote(Note.Aflat)).toEqual("A♭");
+  expect(renderNote(Note.B)).toEqual("B");
 
-  expect(renderNote("E#")).toBe("E♯");
-  expect(renderNote("Fb")).toBe("F♭");
+  expect(renderNote("E#")).toEqual("E♯");
+  expect(renderNote("Fb")).toEqual("F♭");
+});
+
+test("creates pitched notes", () => {
+  const c4 = PitchedNote.fromNote(Note.C);
+  expect(c4.note).toEqual(Note.C);
+  expect(c4.octave).toEqual(4);
+
+  const d3 = PitchedNote.fromNote(Note.D, 3);
+  expect(d3.note).toEqual(Note.D);
+  expect(d3.octave).toEqual(3);
+
+  const aSharpNeg = PitchedNote.fromNote(Note.Asharp, -1);
+  expect(aSharpNeg.note).toEqual(Note.Asharp);
+  expect(aSharpNeg.octave).toEqual(-1);
+
+  const bFlat9 = PitchedNote.fromNote(Note.Bflat, 9);
+  expect(bFlat9.note).toEqual(Note.Bflat);
+  expect(bFlat9.octave).toEqual(9);
+
+  const gFlatFromFSharp = PitchedNote.fromNote(Note.Fsharp, 4, true);
+  expect(gFlatFromFSharp.note).toEqual(Note.Gflat);
+  expect(gFlatFromFSharp.octave).toEqual(4);
+});
+
+test("converts between strings and pitched notes", () => {
+  expect(PitchedNote.parse("C").toString()).toEqual("C4");
+  expect(PitchedNote.parse("A4").toString()).toEqual("A4");
+  expect(PitchedNote.parse("C#-1").toString()).toEqual("C#-1");
+  expect(PitchedNote.parse("Db9").toString()).toEqual("Db9");
+});
+
+test("adds semitones to pitched notes", () => {
+  expect(PitchedNote.parse("C4").addSemitones(0).toString()).toEqual("C4");
+  expect(PitchedNote.parse("C4").addSemitones(1).toString()).toEqual("C#4");
+  expect(PitchedNote.parse("C4").addSemitones(-1).toString()).toEqual("B3");
+  expect(PitchedNote.parse("C4").addSemitones(12).toString()).toEqual("C5");
+  expect(PitchedNote.parse("G7").addSemitones(3).toString()).toEqual("A#7");
+  expect(PitchedNote.parse("G#3").addSemitones(2).toString()).toEqual("A#3");
+
+  // Smallest possible value.
+  expect(PitchedNote.parse("C-1").addSemitones(0).toString()).toEqual("C-1");
+  expect(PitchedNote.parse("C-1").addSemitones(-1).toString()).toEqual("B-1");
+  expect(PitchedNote.parse("C-1").addSemitones(-24).toString()).toEqual("C-1");
+
+  // Largest possible value.
+  expect(PitchedNote.parse("B9").addSemitones(0).toString()).toEqual("B9");
+  expect(PitchedNote.parse("B9").addSemitones(1).toString()).toEqual("C9");
+  expect(PitchedNote.parse("B9").addSemitones(24).toString()).toEqual("B9");
+
+  // Uses flat for root notes which use flats in major scale.
+  expect(PitchedNote.parse("F4").addSemitones(1).toString()).toEqual("Gb4");
+  expect(PitchedNote.parse("Gb4").addSemitones(2).toString()).toEqual("Ab4");
+});
+
+test("makes a chord from a pitched notes", () => {
+  const C = PitchedNote.parse("C4").makeChord([0, 4, 7]);
+  expect(C.map((note) => note.toString())).toEqual(["C4", "E4", "G4"]);
+  const Am = PitchedNote.parse("A4").makeChord([0, 3, 7]);
+  expect(Am.map((note) => note.toString())).toEqual(["A4", "C5", "E5"]);
+  const B = PitchedNote.parse("B3").makeChord([0, 4, 7]);
+  expect(B.map((note) => note.toString())).toEqual(["B3", "D#4", "F#4"]);
 });
 
 test("parses beats", () => {
-  expect(parseBeats(".")).toBe(1);
-  expect(parseBeats(".....")).toBe(5);
-  expect(parseBeats(".,")).toBe(1.5);
-  expect(parseBeats(".:")).toBe(1.25);
-  expect(parseBeats(".;")).toBe(1.125);
+  expect(parseBeats(".")).toEqual(1);
+  expect(parseBeats(".....")).toEqual(5);
+  expect(parseBeats(".,")).toEqual(1.5);
+  expect(parseBeats(".:")).toEqual(1.25);
+  expect(parseBeats(".;")).toEqual(1.125);
 
   expect(parseBeats("'")).toBeCloseTo(2 / 3);
   expect(parseBeats(`"`)).toBeCloseTo(1 / 3);
   expect(parseBeats("'''")).toBeCloseTo(2);
   expect(parseBeats(`"""`)).toBeCloseTo(1);
 
-  expect(parseBeats("")).toBe(4);
-  expect(parseBeats("_")).toBe(4);
-  expect(parseBeats("__")).toBe(8);
-  expect(parseBeats("", /*beatsInBar=*/ 3)).toBe(3);
-  expect(parseBeats("_", /*beatsInBar=*/ 3)).toBe(3);
+  expect(parseBeats("")).toEqual(4);
+  expect(parseBeats("_")).toEqual(4);
+  expect(parseBeats("__")).toEqual(8);
+  expect(parseBeats("", /*beatsInBar=*/ 3)).toEqual(3);
+  expect(parseBeats("_", /*beatsInBar=*/ 3)).toEqual(3);
 
-  expect(parseBeats("C..")).toBe(2);
+  expect(parseBeats("C..")).toEqual(2);
 });
 
 test("sums beats", () => {
-  expect(sumBeats([])).toBe(0);
-  expect(sumBeats([1, 1])).toBe(2);
-  expect(sumBeats([1, 1.5, 0.25, 3])).toBe(5.75);
-  expect(sumBeats([1 / 3, 2 / 3])).toBe(1);
-  expect(sumBeats([1 / 8, 1 / 3, 1 / 2, 1 / 3, 3 / 8, 1 / 3])).toBe(2);
+  expect(sumBeats([])).toEqual(0);
+  expect(sumBeats([1, 1])).toEqual(2);
+  expect(sumBeats([1, 1.5, 0.25, 3])).toEqual(5.75);
+  expect(sumBeats([1 / 3, 2 / 3])).toEqual(1);
+  expect(sumBeats([1 / 8, 1 / 3, 1 / 2, 1 / 3, 3 / 8, 1 / 3])).toEqual(2);
 });
--- lib/music/note.ts
+++ lib/music/note.ts
@@ -19,6 +19,7 @@ export enum Note {
   Bflat = "Bb",
 }
 
+/** Identity between sharp and flat notes. */
 export const NOTE_IDENTITY = new Map<Note, Note>([
   [Note.Csharp, Note.Dflat],
   [Note.Dsharp, Note.Eflat],
@@ -32,6 +33,26 @@ export const NOTE_IDENTITY = new Map<Note, Note>([
   [Note.Bflat, Note.Asharp],
 ]);
 
+function isSharp(note: Note) {
+  return note.endsWith("#");
+}
+
+function isFlat(note: Note) {
+  return note.endsWith("b");
+}
+
+/** Converts a sharp note to a flat note if possible. */
+export function toFlat(note: Note): Note {
+  if (isSharp(note)) return NOTE_IDENTITY.get(note);
+  return note;
+}
+
+/** Converts a flat note to a sharp note if possible. */
+export function toSharp(note: Note): Note {
+  if (isFlat(note)) return NOTE_IDENTITY.get(note);
+  return note;
+}
+
 /** Renders a note for display on the web using unicode ♯ and ♭. */
 export function renderNote(note: Note | string): string {
   return note.replace("#", "♯").replace("b", "♭");
@@ -41,6 +62,119 @@ export function renderNote(note: Note | string): string {
 export enum Scale {
   Major = "",
   Minor = "m",
+}
+
+const PARSE_NOTE_RE = new RegExp(
+  String.raw`^(${Object.values(Note).join("|")})(-1|\d)?$`
+);
+
+const NOTE_TO_INT = new Map<Note, number>([
+  [Note.C, 0],
+  [Note.Csharp, 1],
+  [Note.Dflat, 1],
+  [Note.D, 2],
+  [Note.Dsharp, 3],
+  [Note.Eflat, 3],
+  [Note.E, 4],
+  [Note.F, 5],
+  [Note.Fsharp, 6],
+  [Note.Gflat, 6],
+  [Note.G, 7],
+  [Note.Gsharp, 8],
+  [Note.Aflat, 8],
+  [Note.A, 9],
+  [Note.Asharp, 10],
+  [Note.Bflat, 10],
+  [Note.B, 11],
+]);
+
+const INT_TO_NOTE = new Map<number, Note>([
+  [0, Note.C],
+  [1, Note.Csharp],
+  [2, Note.D],
+  [3, Note.Dsharp],
+  [4, Note.E],
+  [5, Note.F],
+  [6, Note.Fsharp],
+  [7, Note.G],
+  [8, Note.Gsharp],
+  [9, Note.A],
+  [10, Note.Asharp],
+  [11, Note.B],
+]);
+
+/** Octave number in scientific notation. */
+export type Octave = -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+/** Represents a note with an octave. */
+export class PitchedNote {
+  private static MAX_NOTE = 12 * 10 - 1;
+  private static MIN_NOTE = -12;
+
+  private constructor(
+    private readonly value: number,
+    private readonly useFlat: boolean
+  ) {}
+
+  /**
+   * Creates a pitched note from a note and octave value. If useFlat is not
+   * specifies, it's default is whether the major scale uses flats.
+   */
+  static fromNote(
+    note: Note,
+    octave: Octave = 4,
+    useFlat?: boolean
+  ): PitchedNote {
+    return new PitchedNote(
+      octave * 12 + NOTE_TO_INT.get(note),
+      useFlat ?? (isFlat(note) || note === Note.F)
+    );
+  }
+
+  /** Parses a PitchedNote from a string. */
+  static parse(note: string): PitchedNote {
+    const match = PARSE_NOTE_RE.exec(note);
+    if (!match) {
+      throw new Error(`Failed to parse "${note}" as PitchedNote.`);
+    }
+    let octave = parseInt(match[2]);
+    if (isNaN(octave)) octave = 4;
+    return PitchedNote.fromNote(match[1] as Note, octave as Octave);
+  }
+
+  /** Returns the note. */
+  get note(): Note {
+    const note = INT_TO_NOTE.get(((this.value % 12) + 12) % 12);
+    if (this.useFlat) return toFlat(note);
+    return note;
+  }
+
+  /** Returns the octave. */
+  get octave(): Octave {
+    return Math.floor(this.value / 12) as Octave;
+  }
+
+  /** Returns a string version, like "C4", "Db8", "A#-1". */
+  toString(): string {
+    return `${this.note}${this.octave}`;
+  }
+
+  /**
+   * Returns a new note pitched note with the given interval. When the note is
+   * a sharp/flat note, uses useFlat note to determine wheter to return the flat
+   * or sharp version.
+   */
+  addSemitones(i: number): PitchedNote {
+    let val = this.value + i;
+    while (val > PitchedNote.MAX_NOTE) val -= 12;
+    while (val < PitchedNote.MIN_NOTE) val += 12;
+    return new PitchedNote(val, this.useFlat);
+  }
+
+  /** Like addSemitones but for an array of semitone intervals. */
+  makeChord(intervals: Array<number>): Array<PitchedNote> {
+    return intervals.map((i) => this.addSemitones(i));
+  }
 }
 
 const BEAT_LENGTHS = new Map<string, number>([
