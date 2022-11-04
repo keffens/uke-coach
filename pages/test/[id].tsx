3424d4a228456121a7393d9bf9ed7797d8433230
Steffen <steffen.kopecki@gmail.com>
2022-11-04 20:52:56 +0100

    more testing

--- pages/test/[id].tsx
+++ pages/test/[id].tsx
@@ -6,36 +6,32 @@ import { initFirebaseAdmin } from "../../lib/server";
 import { assert } from "../../lib/util";
 import { SongData, toSongData } from "../../lib/firebase";
 
-export default function SongPage({ chordPro }: SongData) {
-  const song = Song.fromTokens(tokenize(chordPro));
-  const title = song.metadata.artist
-    ? `${song.metadata.title} by ${song.metadata.artist}`
-    : song.metadata.title;
-  return <Layout title={title}>{song.metadata.title}</Layout>;
+export default function SongPage({ id }: { id: string }) {
+  return <Layout>Test: {id}</Layout>;
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
   console.log(Date(), "getStaticProps");
   assert(typeof context.params?.id === "string", "context.params.id not valid");
-  initFirebaseAdmin();
-  console.log(Date(), "firebase initialized");
-  const data = (
-    await getFirestore().collection("songs").doc(context.params.id).get()
-  ).data();
-  console.log(Date(), "got data");
-  if (!data) return { notFound: true };
-  return { props: toSongData(data) };
+  // initFirebaseAdmin();
+  // console.log(Date(), "firebase initialized");
+  // const data = (
+  //   await getFirestore().collection("songs").doc(context.params.id).get()
+  // ).data();
+  // console.log(Date(), "got data");
+  // if (!data) return { notFound: true };
+  return { props: { id: context.params?.id } };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   console.log(Date(), "getStaticPaths");
-  initFirebaseAdmin();
-  const songs = await getFirestore()
-    .collection("songs")
-    .where("deployed", "==", true)
-    .get();
+  // initFirebaseAdmin();
+  // const songs = await getFirestore()
+  //   .collection("songs")
+  //   .where("deployed", "==", true)
+  //   .get();
   return {
-    paths: songs.docs.map((song) => ({ params: { id: song.id } })),
+    paths: [{ params: { id: "a" } }, { params: { id: "b" } }],
     fallback: "blocking", // Allow generation on demand for new pages.
   };
 };
